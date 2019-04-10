ba0fe355bb79b272b42e38ecc3257c19d34037e5
alumno <alumno@LAB00PC00>
2019-04-09 22:35:40 -0300

    clase6zip

---
+++ clase06eje1re/main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int utn_getString(char *msg, char *msgError,int minimo,int maximo,int reintentos,int *resultado);
+
+int utn_isvalidNumber(char *str);
+
+int utn_getNumber(char *msg, char *msgError,int minimo,int maximo,int reintentos,int resultado);
+
+int main()
+{
+
+
+
+
+}
+
+int utn_getNumber(char *msg, char *msgError,int minimo,int maximo,int reintentos,int *resultado);
+{
+    int retorno = -1;
+    char buffer[18];
+    int bufferInt;
+    if(msg != NULL)//FALTA VALIDAR
+    {
+        if(utn_getString(msg,msgError,1,16,reintentos,buffer)==0);
+        {
+            if(utn_getNumber(buffer))
+            bufferInt = atoi(buffer);
+            if(bufferInt >= minimo && bufferInt <= maximo)
+            {
+                *resultado = bufferInt;
+                retorno = 0;
+            }
+        }
+
+    }
+
+    return retorno;
+}
+
+int utn_isvalidNumber(char *str);
+{
+    return 1;
+
+
+}
+
+int utn_getString(char *msg, char *msgError,int minimo,int maximo,int reintentos,int *resultado);
+{
+    strncpy(resultado, "1234",5);
+    return 0;
+}
